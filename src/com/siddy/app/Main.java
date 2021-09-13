bf95a8582e994a5e084bfc8475881135ab9d18b0
Abi Sidford <asidford@gmail.com>
2021-09-13 13:55:12 +0100

    Added main to project.

---
+++ src/com/siddy/app/Main.java
@@ -0,0 +1,9 @@
+package com.siddy.app;
+
+public class Main {
+    public static void main(String[] args) {
+        Bird myBird = new Bird("Tweety", "Yellow");
+        myBird.speak();
+        myBird.fly();
+    }
+}
